6c3e9cc992a124500d8a0d12f3221f3cefe048c5
chentingdong <chentingdong@gmail.com>
2020-01-22 23:07:21 -0800

    settings page, place holders

---
+++ frontend/poc-ui/app/src/containers/settings.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import { LoaderButton } from '../components/loader-button'
+import { Tab, Nav, Row, Col } from 'react-bootstrap'
+import ChangePassword from './change-password'
+
+function Settings (props) {
+  const [ isLoading, setIsLoading ] = useState(false);
+
+  return (
+    <div className='container mt-4'>
+      <Tab.Container id="left-tabs-example" defaultActiveKey="password">
+        <Row className="row">
+          <Col sm={4}>
+            <Nav variant="pills" className="flex-column">
+              <Nav.Item>
+                <Nav.Link eventKey="password">password
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="email">email</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="phoneNumber">phone number</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="username">username</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="givenName">given name</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="middleName">middle name</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="familyName">family name</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="picture">picture</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="gender">gender</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={8} className="container">
+            <Tab.Content>
+              <Tab.Pane eventKey="password">
+                <ChangePassword props={props}/>
+              </Tab.Pane>
+              <Tab.Pane eventKey="email">
+                TODO: change email form
+              </Tab.Pane>
+              <Tab.Pane eventKey="phoneNumber">
+                TODO: change phone number form
+              </Tab.Pane>
+              <Tab.Pane eventKey="username">
+                TODO: change username form
+              </Tab.Pane>
+              <Tab.Pane eventKey="givenName">
+                TODO: change first name form
+              </Tab.Pane>
+              <Tab.Pane eventKey="middleName">
+                TODO: change middle name form
+              </Tab.Pane>
+              <Tab.Pane eventKey="familyName">
+                TODO: change last name form
+              </Tab.Pane>
+              <Tab.Pane eventKey="picture">
+                TODO: change picture form
+              </Tab.Pane>
+              <Tab.Pane eventKey="gender">
+                TODO: change gender form
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </div>
+  )
+}
+
+export default Settings
