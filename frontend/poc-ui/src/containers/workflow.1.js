66e240a1362d56abfec7d0f479abb9d010186d1f
chentingdong <chentingdong@gmail.com>
2020-02-09 20:42:12 -0800

    trying workflow diagram lib GoJs

---
+++ frontend/poc-ui/src/containers/workflow.1.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import * as go from 'gojs'
+import { ReactDiagram } from 'gojs-react'
+
+function initDiagram() {
+  const $ = go.GraphObject.make;
+  const diagram =
+    $(go.Diagram,
+      {
+        'undoManager.isEnabled': true,
+        'clickCreatingTool.archetypeNodeData': { text: 'new node', color: 'lightblue' },
+        model: $(go.GraphLinksModel,
+          {
+            linkKeyProperty: 'key'
+          })
+      });
+
+  diagram.nodeTemplate =
+    $(go.Node, 'Auto',
+      new go.Binding('location', 'loc', go.Point.parse).makeTwoWay(go.Point.stringify),
+      $(go.Shape, 'RoundedRectangle',
+        { name: 'SHAPE', fill: 'white', strokeWidth: 0 },
+        new go.Binding('fill', 'color')),
+      $(go.TextBlock,
+        { margin: 8, editable: true },
+        new go.Binding('text').makeTwoWay()
+      )
+    );
+
+  return diagram;
+}
+
+function handleModelChange(changes) {
+  alert('GoJS model changed!');
+}
+
+function Workflow1() {
+  return (
+    <div>
+      <ReactDiagram
+        initDiagram={initDiagram}
+        divClassName='diagram-component container'
+        nodeDataArray={[
+          { key: 0, text: 'Alpha', color: 'lightblue', loc: '0 0' },
+          { key: 1, text: 'Beta', color: 'orange', loc: '150 0' },
+          { key: 2, text: 'Gamma', color: '#197441', loc: '0 150' },
+          { key: 3, text: 'Delta', color: 'pink', loc: '150 150' }
+        ]}
+        linkDataArray={[
+          { key: -1, from: 0, to: 1 },
+          { key: -2, from: 0, to: 2 },
+          { key: -3, from: 1, to: 1 },
+          { key: -4, from: 2, to: 3 },
+          { key: -5, from: 3, to: 0 }
+        ]}
+        onModelChange={handleModelChange}
+      />
+    </div>
+  );
+}
+
+export default Workflow1
