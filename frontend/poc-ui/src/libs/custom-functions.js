70855f6334e10de7ab13fdf3aaef459ee9883ca1
chentingdong <chentingdong@gmail.com>
2020-04-05 16:48:56 -0700

    color

--- frontend/poc-ui/src/libs/custom-functions.js
+++ frontend/poc-ui/src/libs/custom-functions.js
@@ -1,55 +1,63 @@
-function formatDate ( date ) {
-  let d = new Date( date );
+function formatDate(date) {
+  let d = new Date(date);
 
-  var dateString = d.getUTCFullYear() + "-"
-    + ( d.getUTCMonth() + 1 ) + "-" + d.getUTCDate();
-
+  var dateString =
+    d.getUTCFullYear() + "-" + (d.getUTCMonth() + 1) + "-" + d.getUTCDate();
 
   return dateString;
 }
 
-function formatTime ( timestamp ) {
-  let t = new Date( timestamp );
-  var timeString = t.getUTCHours() + ":" + t.getUTCMinutes() + ":" + t.getUTCSeconds();
+function formatTime(timestamp) {
+  let t = new Date(timestamp);
+  var timeString =
+    t.getUTCHours() + ":" + t.getUTCMinutes() + ":" + t.getUTCSeconds();
   return timeString;
 }
 
-function formatDateTime ( timestamp ) {
-  let t = new Date( timestamp );
-  var timeString = formatDate( t ) + " " + formatTime( t );
+function formatDateTime(timestamp) {
+  let t = new Date(timestamp);
+  var timeString = formatDate(t) + " " + formatTime(t);
   return timeString;
 }
 
-function stateColor ( state ) {
-  if ( !state ) return '';
+function stateColor(state) {
+  if (!state) return "";
 
-  let colorClass = '';
-  switch ( state.toLowerCase() ) {
-    case 'active': colorClass = 'warning'; break;
-    case 'block': colorClass = 'error'; break;
-    case 'complete': colorClass = 'success'; break;
-    default: colorClass = 'secondary';
+  let colorClass = "";
+  switch (state) {
+    case "Active":
+      colorClass = "green";
+      break;
+    case "Pending":
+      colorClass = "danger";
+      break;
+    case "Complete":
+      colorClass = "success";
+      break;
+    default:
+      colorClass = "warning";
   }
   return colorClass;
 }
 
-function stateIcon ( state ) {
-  if ( !state ) return '';
+function stateIcon(state) {
+  if (!state) return "";
 
-  let icon = '';
-  switch ( state ) {
-    case 'Active': icon = 'bolt'; break;
-    case 'Pending': icon = 'lock'; break;
-    case 'Complete': icon = 'check'; break;
-    default: icon = 'cog';
+  let icon = "";
+  switch (state) {
+    case "Active":
+      icon = "bolt";
+      break;
+    case "Pending":
+      icon = "lock";
+      break;
+    case "Complete":
+      icon = "check";
+      break;
+    default:
+      icon = "cog";
   }
   return icon;
 }
 
-export {
-  formatDate,
-  formatTime,
-  formatDateTime,
-  stateColor,
-  stateIcon
-};
+export { formatDate, formatTime, formatDateTime, stateColor, stateIcon };
