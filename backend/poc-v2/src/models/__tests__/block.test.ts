de1141cff72dd91cfd37929efb3fcb1a06aeb13f
Baiji He <he.baiji@gmail.com>
2020-04-27 11:27:21 -0700

    add Block and State

--- backend/poc-v2/src/models/__tests__/block.test.ts
+++ backend/poc-v2/src/models/__tests__/block.test.ts
@@ -3,6 +3,6 @@ import { Block } from "../block";
 describe("Block", () => {
   it("should return the name of the block", () => {
     const block = new Block("1", "a", "test");
-    expect(request.name).toBe("a");
+    expect(block.name).toBe("a");
   });
 });
