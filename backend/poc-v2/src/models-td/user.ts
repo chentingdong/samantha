436e8aaf6f49b047ffa0c0c820c206b74fc39c4d
chentingdong <chentingdong@gmail.com>
2020-04-28 00:27:51 -0700

    backend context

--- backend/poc-v2/src/models-td/user.ts
+++ backend/poc-v2/src/models-td/user.ts
@@ -1,11 +1,18 @@
+import {Context} from './context'
+
 export class User {
   readonly id: string;
   name: string;
   email?: string;
-  
+
   constructor(id: string, name: string, email?: string) {
     this.id = id;
     this.name = name;
     if (email) this.email = email;
+  }
+
+  static login = () => {
+    const context = Context.getInstance()
+    context.set('isAuthenticated', true);
   }
 }
