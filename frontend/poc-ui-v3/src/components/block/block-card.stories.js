6ada9dfebd8f743460f14e4687e45465c9347882
Baiji He <he.baiji@gmail.com>
2020-05-13 11:06:12 -0700

    block stories use json data

--- frontend/poc-ui-v3/src/components/block/block-card.stories.js
+++ frontend/poc-ui-v3/src/components/block/block-card.stories.js
@@ -1,111 +1,50 @@
-import React from 'react'
-import { BlockCard } from './block-card'
-import { DndProvider } from 'react-dnd'
-import Backend from 'react-dnd-html5-backend'
-import { withKnobs } from '@storybook/addon-knobs'
+import React from "react";
+import { BlockCard } from "./block-card";
+import { DndProvider } from "react-dnd";
+import Backend from "react-dnd-html5-backend";
+import { withKnobs } from "@storybook/addon-knobs";
 
 export default {
-  title: 'Block',
+  title: "Block",
   component: BlockCard,
   decorators: [withKnobs],
-}
+};
 
-const blockLevel2 = {
-  id: '1',
-  name: 'default request, parallel block container',
-  description: 'empty request wrapper',
-  type: 'parallelStages',
-  state: 'pending',
-  requester: '',
-  responders: [],
-  blocks: [
-    {
-      id: '1',
-      name: 'default request, parallel block container',
-      description: 'empty request wrapper',
-      type: 'parallelStages',
-      state: 'pending',
-      requester: '',
-      responders: [],
-      blocks: [
-        {
-          id: 'leaf-1',
-          name: 'collect timesheet',
-          description: 'collect timesheets',
-          type: 'leaf',
-          state: 'active',
-          requester: '',
-          responders: [],
-          blocks: [],
-        },
-        {
-          id: 'leaf-2',
-          name: 'rating',
-          description: 'team rating',
-          type: 'leaf',
-          state: 'active',
-          requester: '',
-          responders: [],
-          blocks: [],
-        },
-        {
-          id: '1',
-          name: 'default request, parallel block container',
-          description: 'empty request wrapper',
-          type: 'parallelStages',
-          state: 'pending',
-          requester: '',
-          responders: [],
-          blocks: [
-            {
-              id: 'leaf-1',
-              name: 'collect timesheet',
-              description: 'collect timesheets',
-              type: 'leaf',
-              state: 'active',
-              requester: '',
-              responders: [],
-              blocks: [],
-            },
-          ],
-        },
-      ],
-    },
-  ],
-}
+import blockStories from "../../../data/storybook-blocks.json";
 
-const blockLevel1 = blockLevel2.blocks[0]
-const blockLeaf = blockLevel1.blocks[0]
-const blockLevel0 = { ...blockLevel1, blocks: [] }
+const blockLevel2 = blockStories[0];
+const blockLevel1 = blockLevel2.blocks[0];
+const blockLeaf = blockLevel1.blocks[0];
+const blockLevel0 = { ...blockLevel1, blocks: [] };
 
 export const leafBlock = () => {
   return (
     <DndProvider backend={Backend}>
       <BlockCard block={blockLeaf} />
     </DndProvider>
-  )
-}
+  );
+};
 
 export const CompositeBlockLevel0 = () => {
   return (
     <DndProvider backend={Backend}>
       <BlockCard block={blockLevel0} />
     </DndProvider>
-  )
-}
+  );
+};
 
 export const CompositeBlockLevel1 = () => {
   return (
     <DndProvider backend={Backend}>
       <BlockCard block={blockLevel1} />
     </DndProvider>
-  )
-}
+  );
+};
 
 export const CompositeBlockLevel2 = () => {
   return (
     <DndProvider backend={Backend}>
       <BlockCard block={blockLevel2} />
     </DndProvider>
-  )
-}
+  );
+};
