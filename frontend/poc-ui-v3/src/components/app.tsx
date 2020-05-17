7afdd941cc95e0d1478626990226f8edf46f887c
Baiji He <he.baiji@gmail.com>
2020-05-16 18:55:20 -0700

    put login email and logout button in navbar

--- frontend/poc-ui-v3/src/components/app.tsx
+++ frontend/poc-ui-v3/src/components/app.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react'
-import Amplify, { Auth, Hub } from 'aws-amplify'
-import { useEffect, useContext } from 'react'
-import { hot } from 'react-hot-loader/root'
-import Routes from './routes/routes'
-import '../assets/scss/app.scss'
-import { Context, initialState } from './context/store'
-import config from '../../configs/config'
-import { getUser, getUsers } from './user'
-import { Container, Row } from 'react-bootstrap'
+import * as React from "react"
+import Amplify, { Auth, Hub } from "aws-amplify"
+import { useEffect, useContext } from "react"
+import { hot } from "react-hot-loader/root"
+import Routes from "./routes/routes"
+import "../assets/scss/app.scss"
+import { Context, initialState } from "./context/store"
+import config from "../../configs/config"
+import { getUser, getUsers } from "./user"
+import { Container, Row, Navbar } from "react-bootstrap"
 
 const App = () => {
   const { state, dispatch } = useContext(Context)
@@ -18,11 +18,11 @@ const App = () => {
     async function checkLogin() {
       const userInfo = await Auth.currentUserPoolUser()
       if (userInfo) {
-        dispatch({ type: 'authenticate', isAuthenticated: true })
+        dispatch({ type: "authenticate", isAuthenticated: true })
         const user = await getUser()
         await dispatch({
-          type: 'set',
-          data: { user: user },
+          type: "set",
+          data: { user },
         })
       }
     }
@@ -34,22 +34,22 @@ const App = () => {
   }
 
   useEffect(() => {
-    Hub.listen('auth', async ({ payload: { event } }) => {
+    Hub.listen("auth", async ({ payload: { event } }) => {
       switch (event) {
-        case 'signIn':
-          dispatch({ type: 'authenticate', isAuthenticated: true })
+        case "signIn":
+          dispatch({ type: "authenticate", isAuthenticated: true })
           const user = await getUser()
-          dispatch({ type: 'set', data: { user: user } })
+          dispatch({ type: "set", data: { user } })
           const users = await getUsers()
-          dispatch({ type: 'set', data: { users: users } })
+          dispatch({ type: "set", data: { users } })
           break
-        case 'signOut':
-          dispatch({ type: 'authenticate', isAuthenticated: false })
-          dispatch({ type: 'set', data: { user: initialState.user } })
-          dispatch({ type: 'set', data: { users: initialState.users } })
+        case "signOut":
+          dispatch({ type: "authenticate", isAuthenticated: false })
+          dispatch({ type: "set", data: { user: initialState.user } })
+          dispatch({ type: "set", data: { users: initialState.users } })
           break
-        case 'signIn_failure':
-          console.error('user sign in failed')
+        case "signIn_failure":
+          // console.error("user sign in failed")
           break
         default:
           break
@@ -59,11 +59,18 @@ const App = () => {
 
   return (
     <div className="app wrapper vh-100">
-      {state.isAuthenticated && 
-        <button className="btn btn-link" onClick={logout}>
-          {state?.user?.attributes?.email} Logout
-        </button>
-      }
+      {state.isAuthenticated && (
+        <Navbar className="nav justify-content-center">
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>{state?.user?.attributes?.email}</Navbar.Text>
+            <Navbar.Text>
+              <button className="btn btn-link" onClick={logout}>
+                Logout
+              </button>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>
+      )}
       <Routes />
     </div>
   )
