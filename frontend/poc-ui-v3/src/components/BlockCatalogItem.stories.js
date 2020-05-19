ac16eb0e185ad0c87c0a7bb1c063e65901cfe819
Baiji He <he.baiji@gmail.com>
2020-05-19 14:59:40 -0700

    fix stories

--- frontend/poc-ui-v3/src/components/BlockCatalogItem.stories.js
+++ frontend/poc-ui-v3/src/components/BlockCatalogItem.stories.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { BlockCatalogItem } from "./BlockCatalogItem"
+import { DndProvider } from "react-dnd"
+import Backend from "react-dnd-html5-backend"
 
 import blockStories from "../../data/storybook-blocks.json"
 const blockLevel2 = blockStories[0]
@@ -12,19 +14,35 @@ export default {
 }
 
 const BlockCatalogItemLeaf = () => {
-  return <BlockCatalogItem block={blockLeaf} />
+  return (
+    <DndProvider backend={Backend}>
+      <BlockCatalogItem block={blockLeaf} />
+    </DndProvider>
+  )
 }
 
 const BlockCatalogItemLv0 = () => {
-  return <BlockCatalogItem block={blockLevel0} />
+  return (
+    <DndProvider backend={Backend}>
+      <BlockCatalogItem block={blockLevel0} />
+    </DndProvider>
+  )
 }
 
 const BlockCatalogItemLv1 = () => {
-  return <BlockCatalogItem block={blockLevel1} />
+  return (
+    <DndProvider backend={Backend}>
+      <BlockCatalogItem block={blockLevel1} />
+    </DndProvider>
+  )
 }
 
 const BlockCatalogItemLv2 = () => {
-  return <BlockCatalogItem block={blockLevel2} />
+  return (
+    <DndProvider backend={Backend}>
+      <BlockCatalogItem block={blockLevel2} />
+    </DndProvider>
+  )
 }
 
 export { BlockCatalogItemLeaf, BlockCatalogItemLv0, BlockCatalogItemLv1, BlockCatalogItemLv2 }
