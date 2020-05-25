baaa9adc6dd988351699d8dca6fb6452128efdc6
chentingdong <chentingdong@gmail.com>
2020-05-25 02:48:14 -0700

    more theming on particles

--- frontend/poc-ui-v4/src/themes/bootstrap/BlockCatalogItem.tsx
+++ frontend/poc-ui-v4/src/themes/bootstrap/BlockCatalogItem.tsx
@@ -14,112 +14,31 @@ type BlockCatalogItemType = {
 const particlesParms = {
   particles: {
     number: {
-      value: 80,
+      value: 20,
       density: {
-        enable: true,
-        value_area: 800,
-      },
-    },
-    color: {
-      value: "#999",
-    },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#000000",
-      },
-      polygon: {
-        nb_sides: 5,
-      },
-      image: {
-        src: "bell.png",
-        width: 10,
-        height: 10,
-      },
-    },
-    opacity: {
-      value: 0.5,
-      random: false,
-      anim: {
         enable: false,
-        speed: 1,
-        opacity_min: 0.1,
-        sync: false,
       },
     },
     size: {
-      value: 10,
+      value: 8,
       random: true,
-      anim: {
-        enable: false,
-        speed: 80,
-        size_min: 0.1,
-        sync: false,
-      },
+    },
+    move: {
+      direction: "random",
+      out_mode: "out",
     },
     line_linked: {
       enable: false,
-      distance: 300,
-      color: "#ffffff",
-      opacity: 0.4,
-      width: 2,
-    },
-    move: {
-      enable: true,
-      speed: 12,
-      direction: "none",
-      random: false,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 1200,
-      },
     },
   },
   interactivity: {
-    detect_on: "canvas",
     events: {
       onhover: {
-        enable: false,
-        mode: "repulse",
-      },
-      onclick: {
         enable: true,
-        mode: "push",
-      },
-      resize: true,
-    },
-    modes: {
-      grab: {
-        distance: 800,
-        line_linked: {
-          opacity: 1,
-        },
-      },
-      bubble: {
-        distance: 800,
-        size: 80,
-        duration: 2,
-        opacity: 0.8,
-        speed: 3,
-      },
-      repulse: {
-        distance: 400,
-        duration: 0.4,
-      },
-      push: {
-        particles_nb: 4,
-      },
-      remove: {
-        particles_nb: 2,
+        mode: "repulse",
       },
     },
   },
-  retina_detect: true,
 }
 
 const BlockCatalogItemContainerView: React.FC<BlockCatalogItemType> = ({
@@ -182,6 +101,9 @@ const Styles = styled.div.attrs({})`
     padding: 0.5em;
     color: ${(props) => props.theme.fg};
     background: ${(props) => props.theme.bg};
+  }
+  .particles {
+    background: rgba(0, 0, 0, 0.2);
   }
   .close {
     position: absolute;
