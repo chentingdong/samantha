d9d1ae9f2ce9ac5a9513a43851f506f76919c7b5
Baiji He <he.baiji@gmail.com>
2020-05-30 15:43:12 -0700

    move Select to styled. fix remove hover style

--- frontend/poc-ui-v4/src/components/BlockEditor.tsx
+++ frontend/poc-ui-v4/src/components/BlockEditor.tsx
@@ -14,7 +14,7 @@ import {
   ControlLabel,
   TagPicker,
 } from "rsuite"
-import Select from "react-select"
+import { Select } from "./Select"
 import { Button } from "./Button"
 import styled from "styled-components"
 import tw from "tailwind.macro"
@@ -227,20 +227,6 @@ const BlockEditor = styled(BlockEditorRaw)`
   }
   .tree {
     background var(--color-bg-secondary);
-  }
-  .react-select__control,
-  .react-select__menu {
-    background var(--color-bg-secondary);
-  }
-  .react-select__multi-value {
-    background var(--color-bg-default);
-  }
-  .react-select__input,
-  .react-select__multi-value__label {
-    color var(--color-text-primary);
-  }
-  .react-select__option--is-focused {
-    background var(--color-bg-default);
   }
 `
 
