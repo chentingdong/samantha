b8114a2d1a5128cd542835b84d35814d06fe111a
Baiji He <he.baiji@gmail.com>
2020-06-02 21:57:44 -0700

    add one block

--- frontend/poc-ui-v4/src/components/BlockChildrenList.tsx
+++ frontend/poc-ui-v4/src/components/BlockChildrenList.tsx
@@ -6,6 +6,12 @@ import { MutationType } from "../models/enum"
 import tw from "tailwind.macro"
 import styled from "styled-components"
 import { Panel, Notification } from "rsuite"
+import cloneDeep from "lodash/cloneDeep"
+import { GET_USERS } from "../operations/queries/getUsers"
+import { setUiState } from "../operations/mutations/setUiState"
+import { useQuery } from "@apollo/client"
+import { UI_STATE } from "../operations/queries/uiState"
+import { ConsoleSqlOutlined } from "@ant-design/icons"
 
 type BlockChildrenListType = {
   blocks: BlockOrDef[]
@@ -18,23 +24,37 @@ const BlockChildrenListRaw: React.FC<BlockChildrenListType> = ({
   parent,
   type,
 }) => {
+  const { data, loading, error } = useQuery(UI_STATE)
+
+  const findBlock = (root, target) => {
+    if (root.id === target.id) return root
+    let found
+    for (const child of root.children) {
+      found = findBlock(child, target)
+      if (found) return found
+    }
+    return null
+  }
+
+  const addOneBlock = (childBlock) => {
+    const { id, name } = childBlock
+    const { id: pid, name: pname } = parent
+    Notification.info({
+      title: "adding a block",
+      description: `from "${name}" to "${pname}"`,
+    })
+    const newDraftBlock = cloneDeep(data?.uiState?.draftBlock)
+    const newParent = findBlock(newDraftBlock, parent)
+    newParent.children = [...newParent.children, childBlock]
+    setUiState({ draftBlock: newDraftBlock })
+  }
+
   return (
     <Panel shaded collapsible defaultExpanded bodyFill>
       <DndTargetBox
         accept="block"
         greedy={false}
-        onDrop={(childBlock) => {
-          const { id, name } = childBlock
-          const { id: pid, name: pname } = parent
-          Notification.info({
-            title: "adding a block",
-            description: `from ${JSON.stringify(
-              { name },
-              null,
-              2
-            )} to ${JSON.stringify({ pname }, null, 2)}`,
-          })
-        }}
+        onDrop={(childBlock) => addOneBlock(childBlock)}
       >
         {blocks
           .filter((block) => block.__mutation_type__ !== MutationType.Delete)
