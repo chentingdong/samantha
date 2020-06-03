942a3811b36eac2aaa95e941f5e3d61cf534a869
Baiji He <he.baiji@gmail.com>
2020-06-02 22:28:26 -0700

    delete one block

--- frontend/poc-ui-v4/src/components/BlockChildrenItem.tsx
+++ frontend/poc-ui-v4/src/components/BlockChildrenItem.tsx
@@ -6,12 +6,47 @@ import styled from "styled-components"
 import { Icon, Notification } from "rsuite"
 import { getIconByType } from "../utils/Styles"
 import { BlockChildrenList } from "./BlockChildrenList"
+import cloneDeep from "lodash/cloneDeep"
+import { setUiState } from "../operations/mutations/setUiState"
+import { useQuery } from "@apollo/client"
+import { UI_STATE } from "../operations/queries/uiState"
 
 const BlockChildrenItemRaw: React.FC<{
   block: BlockOrDef
   index?: number
 }> = ({ block, index = 0 }) => {
+  const { data, loading, error } = useQuery(UI_STATE)
   const isLeaf = block.type.includes("LEAF_")
+
+  const findBlock = (root, target) => {
+    if (root.id === target.id) return root
+    let found
+    for (const child of root.children) {
+      found = findBlock(child, target)
+      if (found) return found
+    }
+    return null
+  }
+
+  const deleteOneBlock = (childBlock) => {
+    const {
+      id,
+      name,
+      parent: { id: pid, name: pname },
+    } = block
+    Notification.info({
+      title: "deleting a block",
+      description: `"${block.name}" from parent "${block.parent.name}"`,
+    })
+    const newDraftBlock = cloneDeep(data?.uiState?.draftBlock)
+    const newParent = findBlock(newDraftBlock, childBlock.parent)
+    newParent.children.splice(
+      newParent.children.findIndex((child) => child.id === childBlock.id),
+      1
+    )
+    setUiState({ draftBlock: newDraftBlock })
+  }
+
   return (
     <Card className={`${isLeaf ? "leaf" : "composite"} theme-dark`}>
       <DndSourceBox type="block" block={block}>
@@ -21,21 +56,7 @@ const BlockChildrenItemRaw: React.FC<{
           <Icon
             icon="close"
             className="float-right m-1"
-            onClick={() => {
-              const {
-                id,
-                name,
-                parent: { id: pid, name: pname },
-              } = block
-              Notification.info({
-                title: "deleting a block",
-                description: JSON.stringify(
-                  { name, parent: { pname } },
-                  null,
-                  2
-                ),
-              })
-            }}
+            onClick={(e) => deleteOneBlock(block)}
           />
         </div>
         <div className="card-body">{block.description}</div>
