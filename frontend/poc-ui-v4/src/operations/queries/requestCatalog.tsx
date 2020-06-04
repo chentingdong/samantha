8c20c124bd7ef56640a982d28e26cef940370be1
Baiji He <he.baiji@gmail.com>
2020-06-04 14:27:50 -0700

    tweak before demo

--- frontend/poc-ui-v4/src/operations/queries/requestCatalog.tsx
+++ frontend/poc-ui-v4/src/operations/queries/requestCatalog.tsx
@@ -5,12 +5,7 @@ export const REQUEST_CATALOG = gql`
   query requestCatalog {
     blockDefs(
       orderBy: { last_updated: desc }
-      where: {
-        AND: [
-          { OR: [{ type: COMPOSITE_PARALLEL }, { type: COMPOSITE_SEQUENTIAL }] }
-          { parent: null }
-        ]
-      }
+      where: { AND: [{ parent: null }] }
     ) {
       ...BlockDefFullFragment
     }
