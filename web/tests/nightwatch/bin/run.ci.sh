003121cd38be195d199dccc2b9c135b4bfde4131
chentingdong <chentingdong@gmail.com>
2020-06-13 21:14:05 -0700

    fix env

--- web/tests/nightwatch/bin/run.ci.sh
+++ web/tests/nightwatch/bin/run.ci.sh
@@ -1,2 +1,2 @@
-eval $(cat ~/bellhop/samantha/web/tests/nightwatch/.env | sed 's/^/export /')
+eval $(cat tests/nightwatch/.env | sed 's/^/export /')
 nightwatch -c tests/nightwatch/nightwatch.ci.conf.js -f login.js -e chrome_1
