58900e1cec475001b07077b86ccc0dc815520f5f
Baiji He <he.baiji@gmail.com>
2020-06-18 10:37:33 -0700

    dockers

--- hasura/docker-run.sh
+++ hasura/docker-run.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+docker stop $(docker ps --filter publish=5432 -q)
+
+docker run -d --rm --name my_postgres -v my_dbdata:/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_PASSWORD=password POSTGRES_DB=hasura postgres
+
 docker run -d --rm --name my_hasura -p 8080:8080 \
        -e HASURA_GRAPHQL_DATABASE_URL=postgres://postgres:password@host.docker.internal:5432/hasura \
        -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
