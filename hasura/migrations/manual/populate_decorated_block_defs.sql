cdf275d31bce06906f438e1ba9d307a4061b4cc2
chentingdong <chentingdong@gmail.com>
2020-06-23 20:32:49 -0700

    fix data

--- hasura/migrations/manual/populate_decorated_block_defs.sql
+++
@@ -1,8 +0,0 @@
-DELETE FROM public."blockDefs"
-WHERE id='1skauzkTHAOgRKApKaoTd'
-OR id='3GKApRpyTYmgOQ9usbJti'
-OR id='4FKApRpyTYmgOQ9usbJth'
-OR id='6EwwM5Pef77ygZ6qHd6a5'
-OR id='2vwPj9f5K7a21gjv90AOa'
-OR id='5CwwM5Pef77ygZ6qHd6B3'
-
