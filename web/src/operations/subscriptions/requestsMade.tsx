56b70ebb2821aabd21961a8b31c3d59dbb59b3a7
Baiji He <he.baiji@gmail.com>
2020-06-26 12:35:42 -0700

    time to think about query/subscription performance optimization...

--- web/src/operations/subscriptions/requestsMade.tsx
+++ web/src/operations/subscriptions/requestsMade.tsx
@@ -4,6 +4,7 @@ import { blockFullFragment } from "../fragments/block"
 export const REQUESTS_MADE = gql`
   subscription requestsMade($userId: String) {
     blocks(
+      limit: 20
       order_by: { last_updated: desc_nulls_first }
       where: { requestors: { user: { id: { _eq: $userId } } } }
     ) {
