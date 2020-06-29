d1ab590f891a735e864b89473d5fb9eaef078e27
chentingdong <chentingdong@gmail.com>
2020-06-29 09:53:09 -0700

    UpdateBellByPk

--- hasura/src/graphql/__tests__/bells.test.ts
+++ hasura/src/graphql/__tests__/bells.test.ts
@@ -1,5 +1,6 @@
 import { createRandomBellInput } from "./utils"
 import { insertBell } from "../mutations/insertBell"
+import { updateBellByPk } from "../mutations/updateBellByPk"
 import { getBell } from "../queries/getBell"
 import { deleteBellByPk } from "../mutations/deleteBellByPk"
 
@@ -20,6 +21,15 @@ describe("GraphQL", () => {
         console.log(bell.id)
         const result = await getBell(bell.id)
         expect(result.length).toEqual(1)
+      })
+
+      it("should update name", async () => {
+        const name = "updated Bell " + Math.floor(Math.random() * 10)
+        const result = await updateBellByPk({
+          id: bell.id,
+          data: { name: name },
+        })
+        expect(result.name).toEqual(name)
       })
     })
   })
