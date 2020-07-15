39b038b36b304befd70c134f61ee228d0b82c865
chentingdong <chentingdong@gmail.com>
2020-07-15 14:16:30 -0700

    fix storybook build issue

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -1,13 +1,14 @@
+const { resolve } = require("path")
 module.exports = {
   addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-knobs',
-    '@storybook/addon-storysource',
-    '@storybook/addon-viewport/register',
-    '@storybook/addon-notes/register',
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-knobs",
+    "@storybook/addon-storysource",
+    "@storybook/addon-viewport/register",
+    "@storybook/addon-notes/register",
     {
-      name: '@storybook/addon-docs',
+      name: "@storybook/addon-docs",
       options: {
         configureJSX: true,
         babelOptions: {},
@@ -15,25 +16,28 @@ module.exports = {
       },
     },
   ],
-  stories: ['../src/**/*.stories.(js|tsx|mdx)'],
+  stories: ["../src/**/*.stories.(js|tsx|mdx)"],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
-      loader: require.resolve('babel-loader'),
+      loader: require.resolve("babel-loader"),
       options: {
-        presets: [['react-app', { flow: false, typescript: true }]],
+        presets: [["react-app", { flow: false, typescript: true }]],
       },
     })
     config.module.rules.push({
       test: /\.(scss|sass)$/,
       loaders: [
-        'style-loader',
-        { loader: 'css-loader', options: { importLoaders: 1 } },
-        'sass-loader',
+        "style-loader",
+        { loader: "css-loader", options: { importLoaders: 1 } },
+        "sass-loader",
       ],
     })
-
-    config.resolve.extensions.push('.ts', '.tsx')
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      resolve(__dirname, "../src"),
+    ]
+    config.resolve.extensions.push(".ts", ".tsx")
     return config
   },
 }
--- web/.storybook/theme.js
+++ web/.storybook/theme.js
@@ -1,7 +1,7 @@
 // theme.js
 // This file is for storybook page theming, not rsuite theming
 import { create } from "@storybook/theming/create"
-import logo from "../dist/img/bellhop-1000x.png"
+import logo from "../dist/images/bellhop-1000x.png"
 
 export default create({
   base: "light",
