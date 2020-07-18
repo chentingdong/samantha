d448321fd6d49af1beccccc6758909bd233541c2
chentingdong <chentingdong@gmail.com>
2020-07-18 01:09:13 -0700

    my bellhop desk page. need data now

--- web/src/app/M2/MyBellDesk.tsx
+++ web/src/app/M2/MyBellDesk.tsx
@@ -1,7 +1,38 @@
-import * as React from "react"
+// MyBellDesk.tsx
+import React, { useState } from "react"
+import { testingBellhopList } from "../../../data/initialBellhop"
+import { BellhopThumbnailList } from "./BellhopThumbnailList"
+import { BellhopContent } from "./BellhopContent"
+import { useQuery, useSubscription } from "@apollo/client"
+import { UI_STATE } from "../../operations/queries/uiState"
+import { Error } from "../../components/Misc"
+import { Loader } from "rsuite"
 
 interface MyBellDeskProps {}
 
-export const MyBellDesk: React.FC<MyBellDeskProps> = (props) => {
-  return <div>My Bellhop</div>
+const MyBellDesk: React.FC<MyBellDeskProps> = (props) => {
+  // TODO write up query for bellhop list, with fiters
+  // const { loading, error, data } = useQuery(Bellhop)
+  // const [bellhops, setBellhops] = useState(testingBellhopList)
+  const bellhops = testingBellhopList
+  const listTitle = "My Bellhops"
+  const {
+    data: { uiState },
+    loading,
+    error,
+  } = useQuery(UI_STATE)
+
+  if (loading) return <Loader speed="fast" content="Loading..." />
+  if (error) return <Error message={error.message} />
+
+  return (
+    <div className="">
+      {!uiState.currentBellhopId && (
+        <BellhopThumbnailList bellhops={bellhops} listTitle={listTitle} />
+      )}
+      {uiState.currentBellhopId && <BellhopContent listTitle={listTitle} />}
+    </div>
+  )
 }
+
+export { MyBellDesk }
