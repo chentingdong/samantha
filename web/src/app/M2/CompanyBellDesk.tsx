e186576211bc4316b452d3d898e9564455d349c6
chentingdong <chentingdong@gmail.com>
2020-07-17 18:11:19 -0700

    fixed react component import of bellhop item

--- web/src/app/M2/CompanyBellDesk.tsx
+++ web/src/app/M2/CompanyBellDesk.tsx
@@ -1,31 +1,9 @@
 import React, { useState } from "react"
 import { testingBellhopList } from "../../../data/initialBellhop"
-import { Button } from "components/Button"
-import styled from "styled-components"
-import tw from "tailwind.macro"
 import { Bellhop } from "models/interface"
+import { BellhopItem } from "./BellHopItem"
 
-interface BellhopItemProps {
-  bellhop: Bellhop
-}
 interface CompanyBellDeskProps {}
-
-const BellhopItem: React.FC<BellhopItemProps> = ({ bellhop, ...props }) => {
-  return (
-    <div className="relative h-32 overflow-hidden border lg:h-48 xl:h-64">
-      <img
-        className="object-cover w-full h-full opacity-25"
-        src={bellhop.profile_image_url}
-        alt=""
-      />
-      <div className="absolute top-0 left-0 flex content-center w-full h-full">
-        <Button className="w-4/5 m-auto fill" color="primary">
-          {bellhop.name}
-        </Button>
-      </div>
-    </div>
-  )
-}
 
 const CompanyBellDesk: React.FC<CompanyBellDeskProps> = (props) => {
   // TODO write up query for bellhop list, with fiters
