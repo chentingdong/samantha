9a12daae7b266b5550b9ea57b0b234dcdffac3f3
Baiji He <he.baiji@gmail.com>
2020-07-20 16:28:03 -0700

    m2 goals and human participations

--- hasura/seeds/1595268434964_block_state_seed.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO m2.block_state (state, comment) VALUES ('Created', NULL);
-INSERT INTO m2.block_state (state, comment) VALUES ('Running', NULL);
-INSERT INTO m2.block_state (state, comment) VALUES ('Success', NULL);
-INSERT INTO m2.block_state (state, comment) VALUES ('Failure', NULL);
