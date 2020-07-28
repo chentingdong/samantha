6aa323cab8a0ebe5a50da81f5b4bbaae7b462108
chentingdong <chentingdong@gmail.com>
2020-07-27 22:48:04 -0700

    update bellhops data

---
+++ hasura/migrations/1595915228476_update_bellhops_data/up.sql
@@ -0,0 +1,9 @@
+UPDATE m2.bellhops SET profile_image_url='https://samantha-assets.s3.amazonaws.com/images/facilities.png'
+WHERE name='Facilities';
+UPDATE m2.bellhops SET profile_image_url='https://samantha-assets.s3.amazonaws.com/images/finance.png'
+WHERE name='Finance';
+UPDATE m2.bellhops SET profile_image_url='https://samantha-assets.s3.amazonaws.com/images/marketing.png'
+WHERE name='Marketing';
+UPDATE m2.bellhops SET profile_image_url='https://samantha-assets.s3.amazonaws.com/images/marketing.png'
+WHERE name='Legal';
+INSERT INTO m2.bellhops (id, name, profile_image_url) values ('PG4zNBGpm_MNfVU0tMNwt', 'Engineering & Product', 'https://samantha-assets.s3.amazonaws.com/images/eng.png');
