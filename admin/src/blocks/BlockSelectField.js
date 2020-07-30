9771145f63ff66e3acd920cc18af81e597ef9326
Baiji He <he.baiji@gmail.com>
2020-07-30 10:40:59 -0700

    adjust block select field

--- admin/src/blocks/BlockSelectField.js
+++ admin/src/blocks/BlockSelectField.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const BlockSelectField = ({ record }) => (
-  <div> {`${record.type} ${record.name || ""} (${record.id})`} </div>
+  <div> {`${record.name || ""} (${record.id})`} </div>
 );
 
 export default BlockSelectField;
