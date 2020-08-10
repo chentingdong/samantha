ba78113b174d7b85d8b4d4458886d21b54879455
chentingdong <chentingdong@gmail.com>
2020-08-09 21:58:38 -0700

    fix todo magic

--- web/src/components/Todo.tsx
+++ web/src/components/Todo.tsx
@@ -17,7 +17,7 @@ const TODO: React.FC<TODOProps> = ({
   return (
     <div
       className={`${className} relative ${
-        show ? "opacity-75 hover:opacity-100" : "opacity-0"
+        showTodo ? "opacity-75 hover:opacity-100" : "opacity-0"
       }`}
     >
       <div className={`${className} bg-yellow-500 absolute ${position}-0`}>
@@ -26,8 +26,8 @@ const TODO: React.FC<TODOProps> = ({
       <MagicWord
         showWord="todo"
         hideWord="done"
-        show={show}
-        setShow={showTodo}
+        show={showTodo}
+        setShow={setShowTodo}
       ></MagicWord>
     </div>
   )
