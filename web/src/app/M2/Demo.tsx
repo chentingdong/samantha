8957dd3aa6238a73e1bfb18b898c47bed85eb751
chentingdong <chentingdong@gmail.com>
2020-08-14 16:11:01 -0700

    fix production build rsuite issue

--- web/src/app/M2/Demo.tsx
+++ web/src/app/M2/Demo.tsx
@@ -1,19 +1,14 @@
-import React, { useEffect } from "react"
-
 import { Bell } from "./Bell"
 import { BellhopDesk } from "./BellhopDesk"
 import { Lobby } from "./Lobby"
 import { MainMenu } from "./MainMenu"
+import React from "react"
 import { getRouteParams } from "utils/router"
-import { injectRsuiteStyle } from "utils/styles"
 import { useLocation } from "react-router-dom"
 
 interface DemoProps {}
 
 export const Demo: React.FC<DemoProps> = (props) => {
-  useEffect(() => {
-    injectRsuiteStyle("bell")
-  }, [])
   const location = useLocation()
   const params = getRouteParams(location.pathname)
   return (
