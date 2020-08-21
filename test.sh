60b09ad3e7578e3c3bfb5bcf8bdce1f5a2ea6a68
chentingdong <chentingdong@gmail.com>
2020-08-20 17:33:08 -0700

    mattermost

---
+++ test.sh
@@ -0,0 +1,3 @@
+curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "Hello, this is some text\nThis is more text. :tada:"}' http://localhost:4000/hooks/g8qw593d83ddxrjycyfknk7a6y
+
+curl -i -d "{\"login_id\":\"tchen@bellhop.io\",\"password\":\"\"}" -H 'Content-Type: application/json' http://localhost:4000/api/v4/channels
